8567eacc43ebf8bd5d8c302234a6b707ab2f2c89
Ludovic.lafforgue.c@gmail.com <ludovic.lafforgue.c@gmail.com>
2025-09-27 19:18:53 +0200

    weePlan#2 - Préparation Login et redirect

---
+++ app/_layout.tsx
@@ -0,0 +1,11 @@
+import { View, Text, Pressable } from "react-native";
+import { Link } from "expo-router";
+
+export default function Login() {
+
+    return(
+        <View>
+            
+        </View>
+    )
+}
